4d3c87a8729a48b369e6d96c71bba7a40bdf96e6
Muripng <smurillopng@gmail.com>
2023-03-10 10:14:42 -0300

    organizando pastas e adicionando osts que faltavam

--- js/soundManager.js
+++ js/soundManager.js
@@ -5,6 +5,7 @@ dsSound.addEventListener("mouseover", playDsSound);
 dsSound.addEventListener("mouseout", stopDsSound);
 
 function playDsSound() {
+  dsOst.volume = 0.1;
   dsOst.play();
 }
 
@@ -20,6 +21,7 @@ tSound.addEventListener("mouseover", playTSound);
 tSound.addEventListener("mouseout", stopTSound);
 
 function playTSound() {
+  tOst.volume = 0.1;
   tOst.play();
 }
 
@@ -35,6 +37,7 @@ b2Sound.addEventListener("mouseover", playB2Sound);
 b2Sound.addEventListener("mouseout", stopB2Sound);
 
 function playB2Sound() {
+  b2Ost.volume = 0.1;
   b2Ost.play();
 }
 
@@ -50,6 +53,7 @@ isaacSound.addEventListener("mouseover", playIsaacSound);
 isaacSound.addEventListener("mouseout", stopIsaacSound);
 
 function playIsaacSound() {
+  isaacOst.volume = 0.1;
   isaacOst.play();
 }
 
@@ -65,10 +69,59 @@ persona4Sound.addEventListener("mouseover", playPersona4Sound);
 persona4Sound.addEventListener("mouseout", stopPersona4Sound);
 
 function playPersona4Sound() {
+  persona4Ost.volume = 0.1;
   persona4Ost.play();
 }
 
 function stopPersona4Sound() {
   persona4Ost.pause();
   persona4Ost.currentTime = 0;
+}
+
+const aqw3dSound = document.getElementsByClassName("aqw3d")[0];
+const aqw3dOst = document.getElementById("aqw3d-ost");
+
+aqw3dSound.addEventListener("mouseover", playAqw3dSound);
+aqw3dSound.addEventListener("mouseout", stopAqw3dSound);
+
+function playAqw3dSound() {
+  aqw3dOst.volume = 0.1;
+  aqw3dOst.play();
+}
+
+function stopAqw3dSound() {
+  aqw3dOst.pause();
+  aqw3dOst.currentTime = 0;
+}
+
+const kurtzpelSound = document.getElementsByClassName("kurtzpel")[0];
+const kurtzpelOst = document.getElementById("kurtzpel-ost");
+
+kurtzpelSound.addEventListener("mouseover", playKurtzpelSound);
+kurtzpelSound.addEventListener("mouseout", stopKurtzpelSound);
+
+function playKurtzpelSound() {
+  kurtzpelOst.volume = 0.1;
+  kurtzpelOst.play();
+}
+
+function stopKurtzpelSound() {
+  kurtzpelOst.pause();
+  kurtzpelOst.currentTime = 0;
+}
+
+const dmoSound = document.getElementsByClassName("dmo")[0];
+const dmoOst = document.getElementById("dmo-ost");
+
+dmoSound.addEventListener("mouseover", playDmoSound);
+dmoSound.addEventListener("mouseout", stopDmoSound);
+
+function playDmoSound() {
+  dmoOst.volume = 0.1;
+  dmoOst.play();
+}
+
+function stopDmoSound() {
+  dmoOst.pause();
+  dmoOst.currentTime = 0;
 }
