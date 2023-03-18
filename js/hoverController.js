f9fd6c9be945b5a5c5fe4d3252fd5738c32ac375
Sérgio Murillo <smurillopng@gmail.com>
2023-03-18 03:16:20 -0300

    Removendo imagens de background hover
    não deu muito certo

--- js/hoverController.js
+++ js/hoverController.js
@@ -1,27 +1,23 @@
 function playSound(sound, ost, background) {
   const soundElement = document.querySelector(`.${sound}`);
   const ostElement = document.getElementById(ost);
-  const backgroundUrl = `url(${background})`;
-  const bgColor = getComputedStyle(document.body).getPropertyValue('--bg-color');
 
   soundElement.addEventListener("mouseover", () => {
-    document.body.style.backgroundImage = backgroundUrl;
     ostElement.volume = 0.1;
     ostElement.play();
   });
 
   soundElement.addEventListener("mouseout", () => {
-    document.body.style.backgroundImage = bgColor;
     ostElement.pause();
     ostElement.currentTime = 0;
   });
 }
 
-playSound("ds", "ds-ost", "images/background_ds.jpg");
-playSound("terraria", "terraria-ost", "images/background_terraria.jpg");
-playSound("borderlands2", "borderlands2-ost", "images/background_bl2.jpg");
-playSound("isaac", "isaac-ost", "images/background_isaac.jpg");
-playSound("persona4", "persona4-ost", "images/background_p4.jpg");
-playSound("aqw3d", "aqw3d-ost", "images/background_aq3d.jpg");
-playSound("kurtzpel", "kurtzpel-ost", "images/background_kurtzpel.jpg");
-playSound("dmo", "dmo-ost", "images/background_dmo.png");
+playSound("ds", "ds-ost");
+playSound("terraria", "terraria-ost");
+playSound("borderlands2", "borderlands2-ost");
+playSound("isaac", "isaac-ost");
+playSound("persona4", "persona4-ost");
+playSound("aqw3d", "aqw3d-ost");
+playSound("kurtzpel", "kurtzpel-ost");
+playSound("dmo", "dmo-ost");
